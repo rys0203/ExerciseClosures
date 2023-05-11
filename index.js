a860386e1388cf8dd0c37870f9f7014d68a5a4e8
michaelgwilliams774 <132536868+michaelgwilliams774@users.noreply.github.com>
2023-05-11 06:31:09 +0000

    Update the comment

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ function callMeMaybe() {
 
 callMeMaybe();
 
-// Because of closure, even though callMe is defined as const, it doesn't get hoisted. setTimeout goes to WebAPI and while it gets back through callback queue and event loop, callMe is created and pushes onto closure. SO we can use it after setTimeout gets done. How cool is this!~
-
+/** 
+Because of closure, even though callMe is defined as const, it doesn't get hoisted. setTimeout goes to WebAPI and while it gets back through callback queue and event loop, callMe is created and pushes onto closure. SO we can use it after setTimeout gets done. How cool is this!~
+*/
 
 // Let me show you Two main benefits of using Closures
 // 1. Memory efficient
