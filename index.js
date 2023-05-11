3d91e84c08ae0d05d75fe5e46f0f005204a3831e
michaelgwilliams774 <132536868+michaelgwilliams774@users.noreply.github.com>
2023-05-11 05:28:16 +0000

    Closure benefit 1 - Memory Efficient

--- index.js
+++ index.js
@@ -8,3 +8,29 @@ function callMeMaybe() {
 callMeMaybe();
 
 // Because of closure, even though callMe is defined as const, it doesn't get hoisted. setTimeout goes to WebAPI and while it gets back through callback queue and event loop, callMe is created and pushes onto closure. SO we can use it after setTimeout gets done. How cool is this!~
+
+
+// Let me show you Two main benefits of using Closures
+// 1. Memory efficient
+function heavyDuty(index) {
+  const bigArray = new Array(7000).fill('😉')
+  console.log('Array created')
+  return bigArray[index]
+}
+// This will create big Array three times
+heavyDuty(688)
+heavyDuty(688)
+heavyDuty(688)
+// This will create big Array only one time, because it's using Closure
+const getHeavyDuty = heavyDuty2()
+getHeavyDuty(123)
+getHeavyDuty(123)
+getHeavyDuty(123)
+
+function heavyDuty2() {
+  const bigArray = new Array(7000).fill('😉')
+  console.log('Array created')
+  return function(index) {
+    return bigArray[index]
+  }
+}
